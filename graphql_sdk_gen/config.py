8104394b68b460a0b601b8a58946d60e69c59d37
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2022-09-19 13:16:55 +0200

    Use dataclass to store settings

--- graphql_sdk_gen/config.py
+++ graphql_sdk_gen/config.py
@@ -1,8 +1,14 @@
+from dataclasses import dataclass, fields
 from pathlib import Path
 
 import toml
 
 from graphql_sdk_gen.exceptions import ConfigFileNotFound, MissingConfiguration
+
+
+@dataclass
+class Settings:
+    schema_path: str
 
 
 def get_config_file_path(file_name: str = "pyproject.toml") -> Path:
@@ -16,13 +22,22 @@ def get_config_file_path(file_name: str = "pyproject.toml") -> Path:
     return file_path
 
 
-def parse_config_file(file_path: Path, key: str = "graphql-sdk-gen") -> dict:
-    """Parse configuration from toml file. Raise exception if section was not found."""
+def parse_config_file(
+    file_path: Path, section_key: str = "graphql-sdk-gen"
+) -> Settings:
+    """Parse configuration from toml file. Raise exception if section or key was not found."""
     config = toml.load(file_path)
+    if section_key not in config:
+        raise MissingConfiguration(f"File {file_path} has no [{section_key}] section.")
+
     try:
-        return config[key]
-    except KeyError as exc:
-        raise MissingConfiguration(f"File {file_path} has no [{key}] section.") from exc
+        return Settings(**config[section_key])
+    except TypeError as exc:
+        expected_fields = {f.name for f in fields(Settings)}
+        missing_fields = expected_fields.difference(config[section_key])
+        raise MissingConfiguration(
+            f"Missing configuration fields: {', '.join(missing_fields)}"
+        ) from exc
 
 
 settings = parse_config_file(get_config_file_path())
