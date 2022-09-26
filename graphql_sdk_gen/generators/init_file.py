638d129e6a9d311f563b42f387de69fe26ee6727
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2022-09-26 16:41:28 +0200

    Add tests to init file generator

--- graphql_sdk_gen/generators/init_file.py
+++ graphql_sdk_gen/generators/init_file.py
@@ -5,20 +5,20 @@ from .utils import generate_import_from
 
 class InitFileGenerator:
     def __init__(self) -> None:
-        self._imports: list = []
+        self.imports: list = []
 
     def add_import(self, modules: list[str], from_: str, level: int = 0) -> None:
         """Add import to be included in init file."""
-        self._imports.append(
+        self.imports.append(
             generate_import_from(modules=modules, from_=from_, level=level)
         )
 
     def generate(self) -> Module:
         """Generate init with imports and public api of package."""
-        module = Module(body=self._imports, type_ignores=[])
-        if self._imports:
+        module = Module(body=self.imports, type_ignores=[])
+        if self.imports:
             constants_names: list[str] = []
-            for import_ in self._imports:
+            for import_ in self.imports:
                 constants_names.extend([n.name for n in import_.names])
             constants_names.sort()
 
@@ -30,7 +30,7 @@ class InitFileGenerator:
                         )
                     ],
                     value=List(elts=[Constant(value=n) for n in constants_names]),
-                    lineno=len(self._imports) + 1,
+                    lineno=len(self.imports) + 1,
                 )
             )
         return module
