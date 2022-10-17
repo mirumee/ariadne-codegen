a2cc6174230a1ed35d327220723ecb609bc89937
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2022-10-17 15:59:28 +0200

    Add tests of base http client

--- graphql_sdk_gen/generators/base_client.py
+++ graphql_sdk_gen/generators/base_client.py
@@ -8,10 +8,12 @@ from pydantic import BaseModel
 
 class BaseClient:
     def __init__(
-        self, base_url: str, client: Optional[httpx.AsyncClient] = None
+        self, base_url: str, http_client: Optional[httpx.AsyncClient] = None
     ) -> None:
         self.base_url = base_url
-        self.client = client if client else httpx.AsyncClient(base_url=base_url)
+        self.http_client = (
+            http_client if http_client else httpx.AsyncClient(base_url=base_url)
+        )
 
     async def __aenter__(self) -> BaseClient:
         return self
@@ -22,7 +24,7 @@ class BaseClient:
         exc_val,
         exc_tb,
     ) -> None:
-        await self.client.aclose()
+        await self.http_client.aclose()
 
     async def execute(
         self, query: str, variables: Optional[dict[str, Any]] = None
@@ -30,7 +32,7 @@ class BaseClient:
         payload: dict[str, Any] = {"query": query}
         if variables:
             payload["variables"] = self._convert_dict_to_json_serializable(variables)
-        return await self.client.post(url="/graphql/", json=payload)
+        return await self.http_client.post(url="/graphql/", json=payload)
 
     def _convert_dict_to_json_serializable(self, dict_: dict[str, Any]):
         return {
