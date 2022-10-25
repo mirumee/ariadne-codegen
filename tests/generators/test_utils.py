e4d6424218a8251180a072ef73bce673501c5dd9
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2022-10-25 16:04:46 +0200

    Add test of removing unused imports

--- tests/generators/test_utils.py
+++ tests/generators/test_utils.py
@@ -1,4 +1,5 @@
 import ast
+from textwrap import dedent
 
 import pytest
 
@@ -33,3 +34,37 @@ def test_ast_to_str_returns_correct_string_without_removing_imports(
     ast_object, expected_result
 ):
     assert ast_to_str(ast_object, False) == expected_result
+
+
+def test_ast_to_str_removes_unused_imports():
+    not_used_imported_class = "Xyz"
+    module = ast.Module(
+        body=[
+            ast.ImportFrom(
+                module="xyz", names=[ast.alias(name=not_used_imported_class)], level=0
+            ),
+            ast.ImportFrom(module="efg", names=[ast.alias(name="Efg")], level=0),
+            ast.ClassDef(
+                name="TestClass",
+                bases=[ast.Name(id="Efg")],
+                keywords=[],
+                body=[ast.Pass()],
+                decorator_list=[],
+            ),
+        ],
+        type_ignores=[],
+    )
+    expected_generated_code = dedent(
+        """
+        from efg import Efg
+
+
+        class TestClass(Efg):
+            pass
+        """
+    ).lstrip()
+
+    generated_code = ast_to_str(module, True)
+
+    assert generated_code == expected_generated_code
+    assert not_used_imported_class not in generated_code
