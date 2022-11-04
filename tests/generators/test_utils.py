95b23e1a569d889ddc78f0daa992074e21d590b8
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2022-11-04 13:22:32 +0100

    Add tests of formatting multiline strings

--- tests/generators/test_utils.py
+++ tests/generators/test_utils.py
@@ -3,7 +3,12 @@ from textwrap import dedent
 
 import pytest
 
-from graphql_sdk_gen.generators.utils import ast_to_str
+from graphql_sdk_gen.generators.utils import (
+    ast_to_str,
+    convert_to_multiline_string,
+    format_multiline_strings,
+    get_variable_indent_size,
+)
 
 
 @pytest.mark.parametrize(
@@ -68,3 +73,43 @@ def test_ast_to_str_removes_unused_imports():
 
     assert generated_code == expected_generated_code
     assert not_used_imported_class not in generated_code
+
+
+@pytest.mark.parametrize(
+    ["source", "expected_indent"],
+    [
+        ("        query = gql('xyz''xyz')", 8),
+        ("    return True", 4),
+        ("def abcd():", 0),
+    ],
+)
+def test_get_variable_indent_size_returns_number_of_spaces_at_the_beginning_of_source(
+    source, expected_indent
+):
+    assert get_variable_indent_size(source) == expected_indent
+
+
+def test_convert_to_multiline_string_removes_string_with_triple_quotes():
+    source = "'abc\\n'def\\n''ghi\\n'"
+    expected = '"""\n    abc\n    def\n    ghi\n    """'
+
+    assert convert_to_multiline_string(source, variable_indent_size=0) == expected
+
+
+def test_format_multiline_strings_returns_code_with_formatted_multiline_strings():
+    source = """
+    async def abcd(self) -> Abcd:
+        query = gql('query abcd {\\n''  query1 {\\n''    field1\\n''  }\\n''}\\n')
+    """
+    expected = '''
+    async def abcd(self) -> Abcd:
+        query = gql("""
+            query abcd {
+              query1 {
+                field1
+              }
+            }
+            """)
+    '''
+
+    assert format_multiline_strings(source) == expected
