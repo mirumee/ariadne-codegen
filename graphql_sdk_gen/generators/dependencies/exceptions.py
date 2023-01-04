3f1333ca633aa517d2f22db1997b0c11a3e5d289
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2023-01-04 09:36:55 +0100

    Add str methods to exceptions

--- graphql_sdk_gen/generators/dependencies/exceptions.py
+++ graphql_sdk_gen/generators/dependencies/exceptions.py
@@ -13,6 +13,9 @@ class GraphQLError(Exception):
         self.extensions = extensions
         self.orginal = orginal
 
+    def __str__(self) -> str:
+        return self.message
+
     @classmethod
     def from_dict(cls, error: dict):
         return cls(
@@ -27,6 +30,9 @@ class GraphQLError(Exception):
 class GraphQLMultiError(Exception):
     def __init__(self, errors: list[GraphQLError]):
         self.errors = errors
+
+    def __str__(self) -> str:
+        return "; ".join(str(e) for e in self.errors)
 
     @classmethod
     def from_errors_dicts(cls, errors_dicts: list[dict]):
