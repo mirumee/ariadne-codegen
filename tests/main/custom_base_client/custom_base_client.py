f92811aa1c414e34f95992d2fa08b6a754c3e186
Huon Wilson <huon@exoflare.io>
2023-02-15 12:34:21 +1100

    Annotate custom_base_client test to pass mypy --strict

--- tests/main/custom_base_client/custom_base_client.py
+++ tests/main/custom_base_client/custom_base_client.py
@@ -1,6 +1,9 @@
+from typing import Any  # this file needs to pass mypy --strict
+
+
 class CustomAsyncBaseClient:
-    async def execute(self, query, variables):
+    async def execute(self, query: Any, variables: Any) -> Any:
         pass
 
-    def get_data(self, response):
+    def get_data(self, response: Any) -> Any:
         pass
