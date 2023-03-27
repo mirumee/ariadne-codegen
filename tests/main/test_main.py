e10eb305729b27a27183d196740ea3ab3657615d
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2023-03-27 16:09:56 +0200

    Move client tests assets to separate directory

--- tests/main/test_main.py
+++ tests/main/test_main.py
@@ -14,6 +14,8 @@ from ariadne_codegen.exceptions import (
 )
 from ariadne_codegen.main import main
 
+CLIENTS_PATH = Path(__file__).parent / "clients"
+GRAPHQL_SCHEMAS_PATH = Path(__file__).parent / "graphql_schemas"
 
 @pytest.fixture(scope="function")
 def project_dir(request, tmp_path):
@@ -58,76 +60,74 @@ def test_main_shows_version():
     [
         (
             (
-                Path(__file__).parent / "example" / "pyproject.toml",
+                CLIENTS_PATH / "example" / "pyproject.toml",
                 (
-                    Path(__file__).parent / "example" / "queries.graphql",
-                    Path(__file__).parent / "example" / "schema.graphql",
+                    CLIENTS_PATH / "example" / "queries.graphql",
+                    CLIENTS_PATH / "example" / "schema.graphql",
                 ),
             ),
             "example_client",
-            Path(__file__).parent / "example" / "expected_client",
+            CLIENTS_PATH / "example" / "expected_client",
         ),
         (
             (
-                Path(__file__).parent / "extended_models" / "pyproject.toml",
+                CLIENTS_PATH / "extended_models" / "pyproject.toml",
                 (
-                    Path(__file__).parent / "extended_models" / "queries.graphql",
-                    Path(__file__).parent / "extended_models" / "schema.graphql",
-                    Path(__file__).parent / "extended_models" / "common_mixins.py",
-                    Path(__file__).parent / "extended_models" / "mixins_a.py",
-                    Path(__file__).parent / "extended_models" / "mixins_b.py",
+                    CLIENTS_PATH / "extended_models" / "queries.graphql",
+                    CLIENTS_PATH / "extended_models" / "schema.graphql",
+                    CLIENTS_PATH / "extended_models" / "common_mixins.py",
+                    CLIENTS_PATH / "extended_models" / "mixins_a.py",
+                    CLIENTS_PATH / "extended_models" / "mixins_b.py",
                 ),
             ),
             "client_with_extended_models",
-            Path(__file__).parent / "extended_models" / "expected_client",
+            CLIENTS_PATH / "extended_models" / "expected_client",
         ),
         (
             (
-                Path(__file__).parent / "custom_files_names" / "pyproject.toml",
+                CLIENTS_PATH / "custom_files_names" / "pyproject.toml",
                 (
-                    Path(__file__).parent / "custom_files_names" / "queries.graphql",
-                    Path(__file__).parent / "custom_files_names" / "schema.graphql",
+                    CLIENTS_PATH / "custom_files_names" / "queries.graphql",
+                    CLIENTS_PATH / "custom_files_names" / "schema.graphql",
                 ),
             ),
             "custom_names_client",
-            Path(__file__).parent / "custom_files_names" / "expected_client",
+            CLIENTS_PATH / "custom_files_names" / "expected_client",
         ),
         (
             (
-                Path(__file__).parent / "custom_base_client" / "pyproject.toml",
+                CLIENTS_PATH / "custom_base_client" / "pyproject.toml",
                 (
-                    Path(__file__).parent / "custom_base_client" / "queries.graphql",
-                    Path(__file__).parent / "custom_base_client" / "schema.graphql",
-                    Path(__file__).parent
-                    / "custom_base_client"
-                    / "custom_base_client.py",
+                    CLIENTS_PATH / "custom_base_client" / "queries.graphql",
+                    CLIENTS_PATH / "custom_base_client" / "schema.graphql",
+                    CLIENTS_PATH / "custom_base_client" / "custom_base_client.py",
                 ),
             ),
             "custom_base_client",
-            Path(__file__).parent / "custom_base_client" / "expected_client",
+            CLIENTS_PATH / "custom_base_client" / "expected_client",
         ),
         (
             (
-                Path(__file__).parent / "custom_scalars" / "pyproject.toml",
+                CLIENTS_PATH / "custom_scalars" / "pyproject.toml",
                 (
-                    Path(__file__).parent / "custom_scalars" / "queries.graphql",
-                    Path(__file__).parent / "custom_scalars" / "schema.graphql",
-                    Path(__file__).parent / "custom_scalars" / "custom_scalars.py",
+                    CLIENTS_PATH / "custom_scalars" / "queries.graphql",
+                    CLIENTS_PATH / "custom_scalars" / "schema.graphql",
+                    CLIENTS_PATH / "custom_scalars" / "custom_scalars.py",
                 ),
             ),
             "custom_scalars_client",
-            Path(__file__).parent / "custom_scalars" / "expected_client",
+            CLIENTS_PATH / "custom_scalars" / "expected_client",
         ),
         (
             (
-                Path(__file__).parent / "inline_fragments" / "pyproject.toml",
+                CLIENTS_PATH / "inline_fragments" / "pyproject.toml",
                 (
-                    Path(__file__).parent / "inline_fragments" / "queries.graphql",
-                    Path(__file__).parent / "inline_fragments" / "schema.graphql",
+                    CLIENTS_PATH / "inline_fragments" / "queries.graphql",
+                    CLIENTS_PATH / "inline_fragments" / "schema.graphql",
                 ),
             ),
             "inline_fragments_client",
-            Path(__file__).parent / "inline_fragments" / "expected_client",
+            CLIENTS_PATH / "inline_fragments" / "expected_client",
         ),
     ],
     indirect=["project_dir"],
@@ -147,23 +147,21 @@ def test_main_generates_correct_package(
     "project_dir, expected_exception",
     [
         (
-            (Path(__file__).parent / "invalid_pyprojects" / "no_section.toml", ()),
+            (CLIENTS_PATH / "invalid_pyprojects" / "no_section.toml", ()),
             MissingConfiguration,
         ),
         (
-            (Path(__file__).parent / "invalid_pyprojects" / "no_files.toml", ()),
+            (CLIENTS_PATH / "invalid_pyprojects" / "no_files.toml", ()),
             InvalidConfiguration,
         ),
         (
             (
-                Path(__file__).parent
+                CLIENTS_PATH
                 / "invalid_pyprojects"
                 / "operation_not_valid_for_schema.toml",
                 (
-                    Path(__file__).parent / "invalid_pyprojects" / "schema.graphql",
-                    Path(__file__).parent
-                    / "invalid_pyprojects"
-                    / "not_valid_query.graphql",
+                    CLIENTS_PATH / "invalid_pyprojects" / "schema.graphql",
+                    CLIENTS_PATH / "invalid_pyprojects" / "not_valid_query.graphql",
                 ),
             ),
             ParsingError,
@@ -183,11 +181,11 @@ def test_main_raises_exception(
     [
         (
             (
-                Path(__file__).parent / "remote_schema" / "pyproject.toml",
-                (Path(__file__).parent / "remote_schema" / "queries.graphql",),
+                CLIENTS_PATH / "remote_schema" / "pyproject.toml",
+                (CLIENTS_PATH / "remote_schema" / "queries.graphql",),
             ),
             "remote_schema_client",
-            Path(__file__).parent / "remote_schema" / "expected_client",
+            CLIENTS_PATH / "remote_schema" / "expected_client",
         )
     ],
     indirect=["project_dir"],
@@ -195,9 +193,9 @@ def test_main_raises_exception(
 def test_main_uses_remote_schema_url_and_remote_schema_headers(
     mocker, project_dir, package_name, expected_package_path
 ):  # pylint: disable=W0613
-    introspection_response_contenct = (
-        Path(__file__).parent.joinpath("remote_schema", "response.json").read_bytes()
-    )
+    introspection_response_contenct = CLIENTS_PATH.joinpath(
+        "remote_schema", "response.json"
+    ).read_bytes()
     mocked_post = mocker.patch(
         "httpx.post",
         return_value=httpx.Response(
@@ -219,14 +217,12 @@ def test_main_uses_remote_schema_url_and_remote_schema_headers(
 def test_main_can_read_config_from_provided_file(tmp_path):
     old_cwd = Path.cwd()
     files_to_copy = (
-        Path(__file__).parent / "custom_config_file" / "config.toml",
-        Path(__file__).parent / "custom_config_file" / "queries.graphql",
-        Path(__file__).parent / "custom_config_file" / "schema.graphql",
+        CLIENTS_PATH / "custom_config_file" / "config.toml",
+        CLIENTS_PATH / "custom_config_file" / "queries.graphql",
+        CLIENTS_PATH / "custom_config_file" / "schema.graphql",
     )
     copy_files(files_to_copy, tmp_path)
-    expected_client_path = (
-        Path(__file__).parent / "custom_config_file" / "expected_client"
-    )
+    expected_client_path = CLIENTS_PATH / "custom_config_file" / "expected_client"
     package_name = "custom_config_client"
 
     os.chdir(tmp_path)
