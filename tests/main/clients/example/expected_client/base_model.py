516c6884b62e40cc98e21407e92eae9c480d57ed
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2023-04-05 11:00:51 +0200

    Change base model file in tests

--- tests/main/clients/example/expected_client/base_model.py
+++ tests/main/clients/example/expected_client/base_model.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Type, Union, get_args, get_origin
 
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic.class_validators import validator
@@ -15,16 +15,36 @@ class BaseModel(PydanticBaseModel):
 
     # pylint: disable=no-self-argument
     @validator("*", pre=True)
-    def decode_custom_scalars(cls, value: Any, field: ModelField) -> Any:
-        decode = SCALARS_PARSE_FUNCTIONS.get(field.type_)
-        if decode and callable(decode):
+    def parse_custom_scalars(cls, value: Any, field: ModelField) -> Any:
+        return cls._parse_custom_scalar_value(value, field.annotation)
+
+    @classmethod
+    def _parse_custom_scalar_value(cls, value: Any, type_: Type[Any]) -> Any:
+        origin = get_origin(type_)
+        args = get_args(type_)
+        if origin is list and isinstance(value, list):
+            return [cls._parse_custom_scalar_value(item, args[0]) for item in value]
+
+        if origin is Union and type(None) in args:
+            sub_type: Any = list(filter(None, args))[0]
+            return cls._parse_custom_scalar_value(value, sub_type)
+
+        decode = SCALARS_PARSE_FUNCTIONS.get(type_)
+        if value and decode and callable(decode):
             return decode(value)
+
         return value
 
     def dict(self, **kwargs: Any) -> Dict[str, Any]:
         dict_ = super().dict(**kwargs)
-        for key, value in dict_.items():
-            serialize = SCALARS_SERIALIZE_FUNCTIONS.get(type(value))
-            if serialize and callable(serialize):
-                dict_[key] = serialize(value)
-        return dict_
+        return {key: self._serialize_value(value) for key, value in dict_.items()}
+
+    def _serialize_value(self, value: Any) -> Any:
+        serialize = SCALARS_SERIALIZE_FUNCTIONS.get(type(value))
+        if serialize and callable(serialize):
+            return serialize(value)
+
+        if isinstance(value, list):
+            return [self._serialize_value(item) for item in value]
+
+        return value
