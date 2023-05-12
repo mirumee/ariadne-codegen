dff11dd3ed367065693ae9f7b458be39f690a95c
Simon Sawert <simon@sawert.se>
2023-05-12 12:50:29 +0200

    fix: Address PR feedback

--- ariadne_codegen/plugins/shorter_results.py
+++ ariadne_codegen/plugins/shorter_results.py
@@ -40,7 +40,9 @@ Or with the shorthand setting:
 """
 
 import ast
-from typing import Union
+from typing import Union, Dict
+from graphql import GraphQLSchema
+from ariadne_codegen.codegen import generate_attribute, generate_return
 
 from ariadne_codegen.plugins.base import (
     Plugin,
@@ -56,6 +58,12 @@ class ShorterResultsPlugin(Plugin):
     All client method that returns type with a single field will be expanded and
     instead return the type of the inner field.
     """
+
+    def __init__(self, schema: GraphQLSchema, config_dict: Dict) -> None:
+        self.class_dict = {}
+        self.extended_imports = {}
+
+        super().__init__(schema, config_dict)
 
     def generate_result_class(
         self,
@@ -103,9 +111,15 @@ class ShorterResultsPlugin(Plugin):
         if len(method_def.body) < 1:
             return super().generate_client_method(method_def)
 
-        # TODO: Can we assume that the return statement is always the last
-        # statement?
-        return_stmt = method_def.body[-1]
+        return_stmts = list(
+            filter(lambda x: isinstance(x, ast.Return), method_def.body)
+        )
+
+        # We currently only support one return path.
+        if len(return_stmts) != 1:
+            return super().generate_client_method(method_def)
+
+        return_stmt = return_stmts[0]
         if not isinstance(return_stmt, ast.Return):
             return super().generate_client_method(method_def)
 
@@ -126,54 +140,47 @@ class ShorterResultsPlugin(Plugin):
 
         single_field = return_class_ast.body[0]
 
-        # Get the field class name, which might be an optional and if so
-        # represented as an `ast.Subscript`.
-        # We need to update the proper return type to be optional as well so we
-        # store that. The ast in the annotation isn't an evaluated node so we
-        # can't return that as is.
-        if isinstance(single_field.annotation, ast.Name):
-            is_optional = False
-            single_field_class = single_field.annotation.id
-        elif isinstance(single_field.annotation, ast.Subscript):
-            is_optional = True
-            single_field_class = single_field.annotation.slice.id
-        else:
-            return super().generate_client_method(method_def)
+        # Traverse the type annotation until we find the inner type. This can
+        # require several iterations if the return type is something like
+        # Optional[List[Any]]
+        return_node, single_field_class = _update_node(single_field.annotation)
 
-        single_field_name = single_field.target.id
+        # Update the return type to be the inner field and ensure we reference
+        # the single field before returning.
+        method_def.returns = return_node
+        method_def.body[-1] = generate_return(
+            value=generate_attribute(
+                value=return_stmt.value, attr=single_field.target.id
+            )
+        )
 
-        # We only want to expand for classes we created. Some queries will
-        # return a type that has a single field which is of `typing.Any` or
-        # other things not generated this iteration so ignore those.
-        # TODO: Can we ensure this in a better way?
+        # After we change the type we also need to import it in the client if
+        # it's one of our generated types so add the extra import as needed.
+        if (
+            method_def.name not in self.extended_imports
+            and single_field_class in self.class_dict
+        ):
+            self.extended_imports[method_def.name] = set()
+            self.extended_imports[method_def.name].add(single_field_class)
+
+        return super().generate_client_method(method_def)
+
+
+def _update_node(node):
+    """
+    Recurse down a node to finner the inner ast.Name. Once found, evaluate the
+    potential literal so it gets unquoted and return the inner name.
+    """
+    if isinstance(node, ast.Name):
         try:
-            single_field_class = ast.literal_eval(single_field_class)
+            node.id = ast.literal_eval(node.id)
         except ValueError:
             # Not a literal
             pass
 
-        if single_field_class not in self.class_dict:
-            return super().generate_client_method(method_def)
+        return node, node.id
 
-        # Update the return type to be the inner field and ensure we reference
-        # the single field before returning.
-        if is_optional:
-            method_def.returns = ast.Subscript(
-                value=ast.Name(id="Optional"), slice=ast.Name(id=single_field_class)
-            )
-        else:
-            method_def.returns = ast.Name(id=single_field_class)
+    if isinstance(node, ast.Subscript):
+        node.slice, node_id = _update_node(node.slice)
 
-        method_def.body[-1] = ast.Attribute(
-            value=return_stmt,
-            attr=single_field_name,
-        )
-
-        # After we change the type we also need to import it from the client so
-        # add the extra import as needed.
-        if method_def.name not in self.extended_imports:
-            self.extended_imports[method_def.name] = set()
-
-        self.extended_imports[method_def.name].add(single_field_class)
-
-        return super().generate_client_method(method_def)
+        return node, node_id
