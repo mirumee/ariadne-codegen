8015cb18e916ec04b8a8f166c6961015d0b54181
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2023-09-07 10:33:36 +0200

    Set BaseModel's config option protected_namespaces to empty tuple

---
+++ tests/client_generators/dependencies/test_base_model.py
@@ -0,0 +1,10 @@
+from ariadne_codegen.client_generators.dependencies.base_model import BaseModel
+
+
+def test_field_with_model_prefix_doesnt_raise_warning(recwarn):
+    class Test(BaseModel):
+        model_field: str
+
+    Test(model_field="test")
+
+    assert len(recwarn) == 0
