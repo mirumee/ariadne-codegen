8393f634f54bcbfd2dbe15db8e4589670d7e335e
Scott Lessans <slessans@gmail.com>
2023-12-21 10:48:42 -0800

    fix scalar data issue in plugin settings

--- ariadne_codegen/config.py
+++ ariadne_codegen/config.py
@@ -38,7 +38,9 @@ def get_client_settings(config_dict: Dict) -> ClientSettings:
     settings_fields_names = {f.name for f in fields(ClientSettings)}
     try:
         section["scalars"] = {
-            name: ScalarData(
+            name: data
+            if isinstance(data, ScalarData)
+            else ScalarData(
                 type_=data["type"],
                 serialize=data.get("serialize"),
                 parse=data.get("parse"),
