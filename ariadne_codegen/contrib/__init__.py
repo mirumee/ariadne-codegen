1013f892ce3d207ce50b0893bfa4b8985fd8f326
Simon Sawert <simon@sawert.se>
2024-03-15 17:14:10 +0100

    Re-export plugin in

--- ariadne_codegen/contrib/__init__.py
+++ ariadne_codegen/contrib/__init__.py
@@ -1,5 +1,11 @@
 from .extract_operations import ExtractOperationsPlugin
+from .no_global_imports import NoGlobalImportsPlugin
 from .no_reimports import NoReimportsPlugin
 from .shorter_results import ShorterResultsPlugin
 
-__all__ = ["ExtractOperationsPlugin", "NoReimportsPlugin", "ShorterResultsPlugin"]
+__all__ = [
+    "ExtractOperationsPlugin",
+    "NoReimportsPlugin",
+    "ShorterResultsPlugin",
+    "NoGlobalImportsPlugin",
+]
