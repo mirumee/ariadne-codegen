f59d2b809bc6fcc5e5c4e3af707109bbf54dc9c5
Damian Czajkowski <d0.czajkowski@gmail.com>
2025-09-29 16:25:03 +0200

    ruff format

--- ariadne_codegen/main.py
+++ ariadne_codegen/main.py
@@ -50,7 +50,7 @@ def client(config_dict):
             url=settings.remote_schema_url,
             headers=settings.remote_schema_headers,
             verify_ssl=settings.remote_schema_verify_ssl,
-            timeout=settings.remote_schema_timeout
+            timeout=settings.remote_schema_timeout,
         )
 
     plugin_manager = PluginManager(
@@ -94,7 +94,7 @@ def graphql_schema(config_dict):
             url=settings.remote_schema_url,
             headers=settings.remote_schema_headers,
             verify_ssl=settings.remote_schema_verify_ssl,
-            timeout=settings.remote_schema_timeout
+            timeout=settings.remote_schema_timeout,
         )
     )
     plugin_manager = PluginManager(
